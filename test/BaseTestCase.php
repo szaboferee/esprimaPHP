6cfc6f87412e37eb94de174cb23549558310f4fa
Ferenc Szabó <szaboferee@gmail.com>
2014-12-20 21:23:02 +0100

    Make different test runners for fixtures

--- test/BaseTestCase.php
+++ test/BaseTestCase.php
@@ -35,51 +35,15 @@ abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
 	 */
 	public function testFixture($code, $expectedTree) {
 
-		$pretty = JSON_PRETTY_PRINT;
-		$pretty = 0;
-		$debug = 0;
-		$options = [
-			'comment' => property_exists($expectedTree, 'comment'),
-			'range' => true,
-			'loc' => true,
-			'tokens' => property_exists($expectedTree, 'tokens'),
-			'raw' => true,
-			'tolerant' => property_exists($expectedTree, 'tolerant'),
-			'source' => null,
-			'attachComment' => $this->hasAttachedComment($expectedTree)
-		];
-
-		if (property_exists($expectedTree, 'tokens') && count($expectedTree->tokens)) {
-			$options['range'] = property_exists($expectedTree->tokens[0], 'range');
-			$options['loc'] = property_exists($expectedTree->tokens[0], 'loc');
+		if (property_exists($expectedTree, 'lineNumber')) {
+			$this->_testError($code, $expectedTree);
+		} else if (property_exists($expectedTree, 'result')) {
+			$this->_testAPI($code, $expectedTree);
+		} else if (is_array($expectedTree)) {
+			$this->_testTokenize($code, $expectedTree);
+		} else {
+			$this->_testParse($code, $expectedTree);
 		}
-		if (property_exists($expectedTree, 'comments') && count($expectedTree->comments)) {
-			$options['range'] = property_exists($expectedTree->comments[0], 'range');
-			$options['loc'] = property_exists($expectedTree->comments[0], 'loc');
-		}
-
-		if ($options['loc']) {
-			$options['source'] = isset($expectedTree->loc->source) ? $expectedTree->loc->source : null;
-		}
-
-		$expectedTree = json_encode($expectedTree, $pretty);
-
-
-		if($debug) {
-		echo "\nO: " . json_encode($options). "\n";
-		echo "\nC: " . $code. "\n";
-		echo "\nE: " . $expectedTree. "\n";
-		}
-		$actualTree = $this->esprima->parse($code, $options);
-		$actualTree = ($options['comment'] || $options['tokens'] || $options['tolerant'])
-			? $actualTree
-			: (isset($actualTree->body[0]) ? $actualTree->body[0] : null);
-		$actualTree = json_encode($actualTree, $pretty);
-
-		if($debug) echo "\nA: " . $actualTree. "\n";
-
-		$this->assertEquals($expectedTree, $actualTree);
-
 	}
 
 	public function getTestFixtures() {
@@ -108,4 +72,69 @@ abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
     }
 	return false;
 }
+
+	private function _testError($code, $expectedTree)
+	{
+		$this->markTestIncomplete();
+	}
+
+	private function _testAPI($code, $expectedTree)
+	{
+		$this->markTestIncomplete();
+	}
+
+	private function _testTokenize($code, $expectedTree)
+	{
+		$this->markTestIncomplete();
+	}
+
+	private function _testParse($code, $expectedTree)
+	{
+		$pretty = JSON_PRETTY_PRINT;
+		$pretty = 0;
+		$debug = 0;
+
+		$options = [
+			'comment' => property_exists($expectedTree, 'comment'),
+			'range' => true,
+			'loc' => true,
+			'tokens' => property_exists($expectedTree, 'tokens'),
+			'raw' => true,
+			'tolerant' => property_exists($expectedTree, 'tolerant'),
+			'source' => null,
+			'attachComment' => $this->hasAttachedComment($expectedTree)
+		];
+
+		if (property_exists($expectedTree, 'tokens') && count($expectedTree->tokens)) {
+			$options['range'] = property_exists($expectedTree->tokens[0], 'range');
+			$options['loc'] = property_exists($expectedTree->tokens[0], 'loc');
+		}
+		if (property_exists($expectedTree, 'comments') && count($expectedTree->comments)) {
+			$options['range'] = property_exists($expectedTree->comments[0], 'range');
+			$options['loc'] = property_exists($expectedTree->comments[0], 'loc');
+		}
+
+		if ($options['loc']) {
+			$options['source'] = isset($expectedTree->loc->source) ? $expectedTree->loc->source : null;
+		}
+
+		$expectedTree = json_encode($expectedTree, $pretty);
+
+
+		if($debug) {
+			echo "\nO: " . json_encode($options). "\n";
+			echo "\nC: " . $code. "\n";
+			echo "\nE: " . $expectedTree. "\n";
+		}
+		$actualTree = $this->esprima->parse($code, $options);
+		$actualTree = ($options['comment'] || $options['tokens'] || $options['tolerant'])
+			? $actualTree
+			: (isset($actualTree->body[0]) ? $actualTree->body[0] : null);
+		$actualTree = json_encode($actualTree, $pretty);
+
+		if($debug) echo "\nA: " . $actualTree. "\n";
+
+		$this->assertEquals($expectedTree, $actualTree);
+
+	}
 } 
